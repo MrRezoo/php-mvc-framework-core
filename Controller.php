a060f7a39eca08ba12c68b0ca7aa3a1dc4e10a05
Mr.Rezoo <rezam578@gmail.com>
2021-07-10 23:47:56 +0430

    Initial commit

---
+++ Controller.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace app\core;
+
+
+use app\core\middlewares\BaseMiddleware;
+
+/**
+ * Class Controller
+ *
+ * @author Mr.Rezoo <rezam578@gmail.com>
+ * @package app\core
+ */
+class Controller
+{
+    public string $layout = 'main';
+    public string $action = '';
+
+    /**
+     * @var BaseMiddleware[]
+     */
+    protected array $middlewares = [];
+
+    public function setLayout($layout)
+    {
+        $this->layout = $layout;
+    }
+
+    public function render($view, $params = [])
+    {
+        return Application::$app->view->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    /**
+     * @return BaseMiddleware[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+
+}
